d317d9cff04eec84989970421568daaf71a34d67
huseyin doğdu <66470348+huso987@users.noreply.github.com>
2023-06-22 22:45:08 +0300

    Kategoricard eklendi

--- deneme2/src/App.vue
+++ deneme2/src/App.vue
@@ -3,12 +3,14 @@ import { RouterLink, RouterView } from 'vue-router'
 import navbarvuea from './components/Navbar.vue'
 import Kateg from './components/Kategoriler.vue'
 import slide from './components/Slider.vue' 
+import card from './components/Kcard.vue'
 </script>
 
 <template>
 <navbarvuea></navbarvuea>
 <Kateg></Kateg>
 <slide></slide>
+<card></card>
 </template>
 
 <style scoped>
---
+++ deneme2/src/components/Kcard.vue
@@ -0,0 +1,106 @@
+<template>
+    <span>
+    <section class="title"> 
+                
+        <span >
+            Aradığın ürün...
+        </span>
+                
+    </section>
+    <section id="box">
+        <span class="img-box">               
+                <a  class="animate-scale adaptive_height" href="https://aquadipolo.com/collections/saat" ><img id="imgbox" data-srcset="//cdn.shopify.com/s/files/1/0485/5264/0672/files/Handdrawn_Icons-03_170x180_6ee8107c-b91a-4ff6-be01-f98ad3656b54_1024x1024_crop_center.png?v=1636064572" alt="Aqua di Polo" class="lazyautosizes ls-is-cached lazyloaded" data-sizes="auto" sizes="355px" srcset="//cdn.shopify.com/s/files/1/0485/5264/0672/files/Handdrawn_Icons-03_170x180_6ee8107c-b91a-4ff6-be01-f98ad3656b54_1024x1024_crop_center.png?v=1636064572">
+                </a>                                
+                <div class="content spotlight-inner">
+                  <a id="katagori1" href="https://aquadipolo.com/collections/saat" class="shop_link text-hover">
+                   Kol Saati
+                   </a>                            
+                 </div>                               
+        </span>
+        <span class="img-box">   
+                <a class="animate-scale adaptive_height" href="https://aquadipolo.com/collections/gozluk" ><img id="imgbox" data-srcset="//cdn.shopify.com/s/files/1/0485/5264/0672/files/Handdrawn_Icons-02_170x180_2746c2c8-c861-46e3-89ee-c6e870c569c3_1024x1024_crop_center.png?v=1636064572" alt="Aqua di Polo" class="lazyautosizes ls-is-cached lazyloaded" data-sizes="auto" sizes="355px" srcset="//cdn.shopify.com/s/files/1/0485/5264/0672/files/Handdrawn_Icons-02_170x180_2746c2c8-c861-46e3-89ee-c6e870c569c3_1024x1024_crop_center.png?v=1636064572">
+                </a> 
+                <div class="content spotlight-inner"> 
+                  <a id="katagori2"  href="https://aquadipolo.com/collections/gozluk" class="shop_link text-hover">
+                   Güneş Gözlüğü
+                   </a>       
+                </div>
+        </span>
+        <span class="img-box">
+                <a class="animate-scale adaptive_height" href="https://aquadipolo.com/collections/parfum" ><img id="imgbox" data-srcset="//cdn.shopify.com/s/files/1/0485/5264/0672/files/Handdrawn_Icons-04_170x180_33b226a9-d0c7-4c1e-b068-c1264ff70bb1_1024x1024_crop_center.png?v=1636064572" alt="Aqua di Polo" class="lazyautosizes ls-is-cached lazyloaded" data-sizes="auto" sizes="355px" srcset="//cdn.shopify.com/s/files/1/0485/5264/0672/files/Handdrawn_Icons-04_170x180_33b226a9-d0c7-4c1e-b068-c1264ff70bb1_1024x1024_crop_center.png?v=1636064572">
+                </a>
+                <div class="content spotlight-inner">
+                  <a id="katagori3"  href="https://aquadipolo.com/collections/parfum" class="shop_link text-hover">
+                  Parfüm
+                  </a>     
+                </div>
+        </span>
+        <span class="img-box">
+                <a class="animate-scale adaptive_height" href="https://aquadipolo.com/collections/canta" ><img id="imgbox" data-srcset="//cdn.shopify.com/s/files/1/0485/5264/0672/files/Handdrawn_Icons-01_170x180_aae444a1-e699-4477-a4a0-c3b104caae14_1024x1024_crop_center.png?v=1636064572" alt="Aqua di Polo" class="lazyautosizes ls-is-cached lazyloaded" data-sizes="auto" sizes="355px" srcset="//cdn.shopify.com/s/files/1/0485/5264/0672/files/Handdrawn_Icons-01_170x180_aae444a1-e699-4477-a4a0-c3b104caae14_1024x1024_crop_center.png?v=1636064572">
+                </a>
+                <div class="content spotlight-inner">
+                    <a id="katagori4"  href="https://aquadipolo.com/collections/canta" class="shop_link text-hover">
+                    Çanta
+                    </a>
+                 </div>
+        </span>
+        <span class="img-box">  
+                <a class="animate-scale adaptive_height" href="https://aquadipolo.com/collections/sweatshirt" ><img id="imgbox" data-srcset="//cdn.shopify.com/s/files/1/0485/5264/0672/files/Handdrawn_Icons-06_170x180_dad3d04d-badf-4f54-843b-691de01a526c_1024x1024_crop_center.png?v=1636064572" alt="Aqua di Polo" class="lazyautosizes ls-is-cached lazyloaded" data-sizes="auto" sizes="355px" srcset="//cdn.shopify.com/s/files/1/0485/5264/0672/files/Handdrawn_Icons-06_170x180_dad3d04d-badf-4f54-843b-691de01a526c_1024x1024_crop_center.png?v=1636064572">
+                </a>
+                <div class="content spotlight-inner">
+                    <a id="katagori5"  href="https://aquadipolo.com/collections/sweatshirt" class="shop_link text-hover">
+                    SWEATSHIRT
+                    </a>              
+                </div>
+                                
+        </span>
+    </section>
+
+  </span>
+
+</template>
+
+<style >
+.title {
+    position: relative;
+    left: 660px;
+    color: black;
+    font-size: 27px;
+    bottom: -17px;
+    font-weight: bold;
+}
+
+.img-box {
+    float: left;
+    margin-left: 20px;
+}
+
+#box {
+    position: relative;
+    left: 150px;
+    width: 1200px;
+    padding-top: 100px;
+}
+#imgbox {
+    height: 220px;
+    
+}
+.shop_link {
+    color: #505050;
+    position: relative;
+    left: 65px;
+    text-decoration-line: none;
+}
+.shop_link:hover {
+    color: #505050;
+    text-decoration-line: underline;
+}
+#katagori4 {
+    position: relative;
+    left: 80px;
+}
+#katagori2 {
+    position: relative;
+    left: 50px;
+}
+</style>
