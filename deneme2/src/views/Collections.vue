2830c9a955b9c05e29ea1bf536ed35fa4bcb8f6b
huseyin doğdu <66470348+huso987@users.noreply.github.com>
2023-06-23 20:45:49 +0300

    Collection ve product sayfası eklendi

---
+++ deneme2/src/views/Collections.vue
@@ -0,0 +1,126 @@
+<template>
+    <div >
+ <header>
+    <Navbar/>
+    <Kategoriler/>
+ </header>
+ <body>
+  <section id="bölüm" class="header-main border-bottom ">
+   <div><nuxt-link  id="asd" to="/">AnaSayfa</nuxt-link></div>
+   <span id="sapr"   
+   class="sidebar_collection-title_custom">
+     {{ ca }}
+   </span>
+   
+   <div id="cont">
+      <div   v-for="(product , index) in data2"
+     
+           :key="'product-'+ index " 
+            
+      id="cardana"
+     
+      >
+      <div  >
+      <nuxt-link  id="carda" v-bind:to="'/Urun/'+(product.id)"  style="width: 18rem;">
+        <div id="cardp" class="card" style="width: 18rem;" >
+           <img :src="product.imgUrl" class="card-img-top" >
+        </div>
+        <p id="pt" >{{product.name}}</p>
+        
+      </nuxt-link>
+ 
+           <p id="pricekat">{{product.price}}</p>
+           <em id="simgetl" >TL</em>
+         </div>
+      </div>
+     
+   </div>
+  </section>
+ </body>
+ </div> 
+ </template>
+ 
+ <style>
+ 
+ #asd {
+  color: rgb(75, 73, 73);
+  text-decoration-line: none;
+  position: relative;
+  left: 185px;
+  font-size: 13px;
+ }
+ #asd:hover {
+ color: #007bff;
+ }
+ .sidebar_collection-title_custom {
+  color: black;
+  font-style: normal;
+  font-size: 20px;
+  position: relative;
+  left: 180px;
+  top: 14px;
+  font-weight: bold;
+  
+ }
+ 
+ #bölüm {
+  height: 80px;
+  
+ }
+ #cont {
+  background-color:white;
+  position: relative;
+  top: 30px;
+  width: 1000px;
+  left: 250px;
+  
+ }
+ .card-img-top {
+  height: 300px;
+ }
+ #pt {
+  color: black;
+  text-decoration-line: none;
+  width: 18rem;
+  font-weight:bold;
+ }
+ #carda {
+  text-decoration-line: none;
+ }
+ #pt:hover {
+  text-decoration: underline black;
+ }
+ #pricekat {
+ float: left;
+ font-weight: bold;
+ }
+ #cardana {
+  margin:20px;
+  float: left;
+ }
+ #simgetl {
+    font-weight: bold;
+ }
+ </style>
+ 
+ <script>
+ 
+ export default defineComponent({
+  setup() {
+  let {data} =useFetch('/api/products');
+  const router =useRoute();
+ 
+  const ca=router.params.Categories;
+ 
+  let data2=data.value.filter((t)=>t.category==ca);
+  
+ 
+  
+    return {data2,ca};
+ 
+  }
+ 
+ })
+ 
+ </script> 
+ 
---
+++ deneme2/src/views/Products.vue
@@ -0,0 +1,117 @@
+<template>
+   
+  <div>
+  <header>
+     <Navbar/>
+     <kategoriler/>
+  </header>
+  <body>
+      <div id="cardPr" class="card mb-3" style="max-width: 1000px; border-color: white;">
+<div   class="row g-0" 
+v-for="(product , index) in data3"
+      :key="'product-'+ index "
+
+>
+  <div  class="col-md-4">
+    <img :src="product.imgUrl" class="img-fluid rounded-start"
+    
+    alt="...">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body" >
+      <div><nuxt-link  id="as" to="/">AnaSayfa</nuxt-link></div>
+      
+        <h5 id="namep" class="card-title">{{product.name}}</h5>
+      <p id="price">{{product.price}}</p>
+          <em id="pricetag" >TL</em>
+          <div class="quantity">
+            <div class="qty-group">
+              <input id="qg" type="number" data-qtt-id="quantity__7265323450528" name="quantity" value="1" data-gtm-form-interact-field-id="6">
+            </div>
+            <input data-btn-addtocart="" type="submit" name="add" class="btnekle" id="product-add-to-cart" value="Sepete ekle" data-form-id="#add-to-cart-form">
+           
+          </div>
+      
+         
+    </div>
+  </div>
+</div>
+</div>
+  </body>
+</div>
+</template>
+
+<style>
+#namep {
+font-weight: bold;
+}
+#as {
+position: relative;
+left: -0px;
+}
+#cardPr {
+position: relative;
+left: 180px;
+
+}
+#price {
+font-weight: bold;
+font-size: 25px;
+position: relative;
+left: 40px;
+top: 4px;
+
+}
+.quantity {
+position: relative;
+top: 50px;
+left: 10px;
+}
+#qg {
+width: 50px;
+
+}
+.btnekle {
+background-color: #293355;
+color: white;
+position: relative;
+height: 50px;
+width: 150px;
+left: 80px;
+top: -35px;
+border: solid black;
+}
+.btnekle:hover {
+background-color: white;
+border: solid black;
+color: #293355;
+}
+#btsearch {
+background-color: white;
+}
+#pricetag {
+position: relative;
+top: -50px;
+font-weight: bold;
+font-size: 25px;
+}
+</style>
+
+<script>
+export default defineComponent({
+setup() {
+let {data} =useFetch('/api/products');
+const router =useRoute();
+console.log(data)
+const ca=router.params.UrunDetay;
+console.log(ca)
+let data3=data.value.filter((t)=>t.id==ca);
+
+
+
+  return {data3,ca};
+
+}
+
+})
+</script>
